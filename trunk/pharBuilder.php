d1f23caad98547739e051af5f422a26ec943f486
maks.slesarenko@gmail.com <maks.slesarenko@gmail.com@31e2b2a5-3e5e-9f24-74ba-4283907349c7>
2011-11-12 19:33:04 +0000

    fixes for pharBuilder.php

--- trunk/pharBuilder.php
+++ trunk/pharBuilder.php
@@ -1,16 +1,12 @@
 <?php
 
-$phar=new Phar('dbsync.phar');
-$phar->buildFromDirectory('DbSync');
-$phar->buildFromDirectory('dependencies');
-$phar->addFile('index.php');
-$phar->addFile('init.php');
-$phar->addFile('phpdbsync.ini.example');
-$phar->addFile('LICENSE.txt');
-$phar->addFile('README.txt');
+$phar = new Phar('dbsync.phar');
+$phar->buildFromDirectory('phar');
 
 $phar->setDefaultStub('index.php');
 
 $signatures=Phar::getSupportedSignatures();
 
 $phar->setSignatureAlgorithm(PHAR::SHA512);
+
+$phar->extractTo('pharTest');
